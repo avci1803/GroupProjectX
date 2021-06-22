156cf3a927ee4148dcb235d4e6c2a058b6aeb7e5
avci1803 <mehmetavci1803@gmail.com>
2021-06-22 18:21:49 +0200

    from local added to github repo

--- src/test/java/TC001.java
+++ src/test/java/TC001.java
@@ -1,5 +1,7 @@
 public class TC001 {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("From local to github");
+        System.out.println("addedd more work from local to githup repo");
     }
 }
